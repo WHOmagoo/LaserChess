dd8ef4f70a6789efe7bd68b2ec9a400a2b638569
hmcgough <hrmagoo@gmail.com>
2021-01-19 00:27:48 -0800

    refactor files for modularized maven project

---
+++ LaserChessGame/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module net.ddns.whomagoo.laserchess.game {
+    requires com.google.gson;
+    exports net.ddns.whomagoo.laserchess.game;
+    exports net.ddns.whomagoo.laserchess.game.move;
+    exports net.ddns.whomagoo.laserchess.game.piece;
+}
--- src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module LaserChess {
-  requires javafx.controls;
-  requires com.google.gson;
-  exports net.ddns.whomagoo.laserchess.gui;
-}
