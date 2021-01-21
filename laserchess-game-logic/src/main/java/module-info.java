ddd2bf85148242e4779e5b722beccf3187568be9
hmcgough <hrmagoo@gmail.com>
2021-01-21 02:38:06 -0800

    Modify package information to allow for unit tests

--- laserchess-game-logic/src/main/java/module-info.java
+++ laserchess-game-logic/src/main/java/module-info.java
@@ -3,4 +3,8 @@ module net.ddns.whomagoo.laserchess.game {
     exports net.ddns.whomagoo.laserchess.game;
     exports net.ddns.whomagoo.laserchess.game.move;
     exports net.ddns.whomagoo.laserchess.game.piece;
+
+    opens net.ddns.whomagoo.laserchess.game;
+    opens net.ddns.whomagoo.laserchess.game.move;
+    opens net.ddns.whomagoo.laserchess.game.piece;
 }
