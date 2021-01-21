0bafe0ab63df5f8de532fcca6d54ece211299d29
hmcgough <hrmagoo@gmail.com>
2021-01-21 02:38:18 -0800

    Add simple unit test

---
+++ laserchess-game-pc/src/test/java/net/ddns/whomagoo/laserchess/gui/pc/AppTest.java
@@ -0,0 +1,17 @@
+package net.ddns.whomagoo.laserchess.gui.pc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue()
+  {
+    assertTrue( true );
+  }
+
+}
